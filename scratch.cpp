3a9031bce349cda13ccb700961cf699afda1d1ba
mostafa-k-m <mostafa.kamal@odiggo.com>
2020-12-25 13:07:55 +0200

    cleanup

--- scratch.cpp
+++ scratch.cpp
@@ -3,15 +3,15 @@
 #include <vector>
 #include <iostream>
 
-
-
-int main() {
+int main()
+{
     PlynomialRegression P(2);
     CSVReader csv = CSVReader("./datasets/part_2/a/2_a_dataset_1.csv");
     P.fit(csv.data[0], csv.data[1]);
     vector<float> y = P.predict(0, 10, 0.1);
-    for (int k = 0; k < 100; k++) {
+    for (int k = 0; k < 100; k++)
+    {
         cout << y[k] << endl;
     }
-    return(0);
+    return (0);
 }
