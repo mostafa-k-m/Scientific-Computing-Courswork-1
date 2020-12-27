6fc4083f37eaa4bf1d2eeb716e1cfe8eb9608009
mostafa-k-m <mostafa.kamal@odiggo.com>
2020-12-27 03:13:18 +0200

    plotting function done

--- Coursework1/PlottingFunctions.h
+++ Coursework1/PlottingFunctions.h
@@ -3,21 +3,23 @@ namespace plt = matplotlibcpp;
 
 class PlotLineScatter
 {
-    vector<double> x;
-    vector<double> y;
-    vector<double> x_scatter;
-    vector<double> y_scatter;
-    string filename;
+public:
+
+    std::vector<double> x;
+    std::vector<double> y;
+    std::vector<double> x_scatter;
+    std::vector<double> y_scatter;
+    std::string filename;
     
     void gen_plot(){
         plt::figure_size(1200, 780);
         plt::plot(x, y);
-        plt::scatter(x_scatter, y_scatter_, 10,{{"color", "red"}});
+        plt::scatter(x_scatter, y_scatter, 10,{{"color", "red"}});
         plt::legend();
         plt::save("./" + filename);
     }
 
-    PlotLineScatter(vector<double>  x_, vector<double>  y_, vector<double>  x_scatter_, vector<double>  y_scatter_, string filename_)
+    PlotLineScatter(std::vector<double>  x_, std::vector<double>  y_, std::vector<double>  x_scatter_, std::vector<double>  y_scatter_, std::string filename_)
     {
         x = x_;
         y = y_;
@@ -25,4 +27,4 @@ class PlotLineScatter
         y_scatter = y_scatter_;
         filename = filename_;
     }
-}
+};
