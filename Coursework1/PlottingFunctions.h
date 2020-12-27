a7eaee41a7f1f49bca22ecc582ed977bacd42528
mostafa-k-m <mostafa.kamal@odiggo.com>
2020-12-27 02:56:00 +0200

    bugs fixed

--- Coursework1/PlottingFunctions.h
+++ Coursework1/PlottingFunctions.h
@@ -19,23 +19,10 @@ class PlotLineScatter
 
     PlotLineScatter(vector<double>  x_, vector<double>  y_, vector<double>  x_scatter_, vector<double>  y_scatter_, string filename_)
     {
-        x_ = x;
-        y_ = y;
-        x_scatter_ = x_scatter;
-        y_scatter_ = y_scatter;
-        filename_ = filename;
+        x = x_;
+        y = y_;
+        x_scatter = x_scatter_;
+        y_scatter = y_scatter_;
+        filename = filename_;
     }
 }
-// Set the size of output image to 1200x780 pixels
-
-// Plot line from given x and y data. Color is selected automatically.
-
-// Plot a red dashed line from given x and y data.
-
-// Plot a line whose name will show up as "log(x)" in the legend.
-// Set x-axis to interval [0,1000000]
-// Add graph title
-plt::title("Sample figure");
-// Enable legend.
-
-// Save the image (file format is determined by the extension)
